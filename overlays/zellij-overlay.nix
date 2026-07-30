8fcc78f3e1ab4a65fb634916d74ddac9eb6a488a
Mohib Mirza <mohib.mirza@mechanical-orchard.com>
2026-07-30 02:24:47 -0700

    chore(zellij): update to 0.44.3

--- overlays/zellij-overlay.nix
+++ overlays/zellij-overlay.nix
@@ -1,13 +1,26 @@
 { mouseScrollLines ? 1 }:
 
 final: prev: {
-  zellij = prev.zellij.overrideAttrs (oldAttrs: {
+  zellij = prev.zellij.overrideAttrs (oldAttrs: rec {
+    version = "0.44.3";
+
+    src = final.fetchFromGitHub {
+      owner = "zellij-org";
+      repo = "zellij";
+      rev = "v${version}";
+      hash = "sha256-r8GAOiau4CZPVotFmsBQJOvEu+t0Bu9UCYAOs18i3Kg=";
+    };
+
+    cargoDeps = final.rustPlatform.importCargoLock {
+      lockFile = "${src}/Cargo.lock";
+    };
+
     postPatch = (oldAttrs.postPatch or "") + ''
-      substituteInPlace zellij-server/src/tab/mod.rs \
-        --replace-fail 'self.handle_scrollwheel_up(&event.position, 3, client_id)' \
-                       'self.handle_scrollwheel_up(&event.position, ${toString mouseScrollLines}, client_id)' \
-        --replace-fail 'self.handle_scrollwheel_down(&event.position, 3, client_id)' \
-                       'self.handle_scrollwheel_down(&event.position, ${toString mouseScrollLines}, client_id)'
+      substituteInPlace zellij-server/src/screen.rs \
+        --replace-fail '.handle_scrollwheel_up(&point, 3, client_id)' \
+                       '.handle_scrollwheel_up(&point, ${toString mouseScrollLines}, client_id)' \
+        --replace-fail '.handle_scrollwheel_down(&point, 3, client_id)' \
+                       '.handle_scrollwheel_down(&point, ${toString mouseScrollLines}, client_id)'
     '';
   });
 }
